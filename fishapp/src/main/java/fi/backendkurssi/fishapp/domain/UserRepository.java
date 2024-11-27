1ad82902b73b186cd42a2470ff6c7b7cf7d65359
Kosti Kangasmaa <kangasmaa95@gmail.com>
2024-11-27 16:05:57 +0200

    Created UserRepository

---
+++ fishapp/src/main/java/fi/backendkurssi/fishapp/domain/UserRepository.java
@@ -0,0 +1,9 @@
+package fi.backendkurssi.fishapp.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+
+public interface UserRepository extends CrudRepository<User, Long> {
+        User findByUsername(String username);
+}
