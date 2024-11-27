9af6e527e06ad034a9ce92c8c3023ccf1d6e3e95
Kosti Kangasmaa <kangasmaa95@gmail.com>
2024-11-27 15:54:08 +0200

    spring initializer folder

---
+++ fishapp/src/test/java/fi/backendkurssi/fishapp/FishappApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.backendkurssi.fishapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
